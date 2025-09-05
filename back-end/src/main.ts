c04a37c9e49a26a6c47c243aa54a8bf55734c3ce
Pouya panahzadeh <Pouyapanahzadeh@gmail.com>
2025-09-05 16:36:24 +0200

    enableCors on backend

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
